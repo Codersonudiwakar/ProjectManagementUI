2c917d4c08e0433075bffa450b2a4b01fce841e3
Codersonudiwakar <138717372+Codersonudiwakar@users.noreply.github.com>
2024-06-07 23:25:50 +0530

    commit

--- src/TopHeader.jsx
+++ src/TopHeader.jsx
@@ -24,17 +24,13 @@ function TopHeader() {
               <NavDropdown.Item href="#action4">
                 Another action
               </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
             </NavDropdown>
             <Nav.Link href="#" disabled>
               Link
             </Nav.Link>
           </Nav>
           <Form className="d-flex">
-          <Button variant="primary">Primary</Button>{' '}
+          
             <Form.Control
               type="search"
               placeholder="Search"
