7fb925d16bd8aa063f151f25e57c53feb46cd922
Codersonudiwakar <138717372+Codersonudiwakar@users.noreply.github.com>
2024-07-05 22:27:26 +0530

    commit

--- src/UserSelect.jsx
+++ src/UserSelect.jsx
@@ -2,24 +2,31 @@ import React, { useState, useEffect } from 'react';
 import Select from 'react-select';
 import { myAxios } from './service/service';
 
-
 const UserSelect = () => {
   const [options, setOptions] = useState([]);
   const [selectedOption, setSelectedOption] = useState(null);
+  const [username, setInputValue] = useState('');
 
   useEffect(() => {
-    myAxios.get('/api/allUser')
-      .then(response => {
-        const userOptions = response.data.map(user => ({
-          value: user.id,
-          label: user.name // Assuming each user object has an id and name
-        }));
-        setOptions(userOptions);
-      })
-      .catch(error => {
-        console.error('Error fetching users:', error);
-      });
-  }, []);
+    if (username) {
+      myAxios.get(`/api/allUser/${username}`)
+        .then(response => {
+          const userOptions = response.data.map(user => ({
+            value: user.id,
+            label: user.name // Assuming each user object has an id and name
+          }));
+          setOptions(userOptions);
+          console.log(userOptions);
+        })
+        .catch(error => {
+          console.error('Error fetching users:', error);
+        });
+    }
+  }, [username]);
+
+  const handleInputChange = (newValue) => {
+    setInputValue(newValue);
+  };
 
   const handleChange = (selected) => {
     setSelectedOption(selected);
@@ -29,6 +36,7 @@ const UserSelect = () => {
     <Select
       value={selectedOption}
       onChange={handleChange}
+      onInputChange={handleInputChange}
       options={options}
       isSearchable={true}
       placeholder="Select a user..."
